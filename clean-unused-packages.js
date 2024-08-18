1736a4df62420a3835c5a389af2bd77625fea373
arnavhavoc <arnav.documentation@gmail.com>
2024-08-19 03:10:29 +0530

    Updated

--- clean-unused-packages.js
+++ clean-unused-packages.js
@@ -2,10 +2,23 @@ const fs = require("fs");
 const path = require("path");
 const { execSync } = require("child_process");
 const glob = require("glob");
+const babelParser = require("@babel/parser");
 
 const projectDir = process.cwd();
 const packageJsonPath = path.join(projectDir, "package.json");
-const nodeModulesPath = path.join(projectDir, "node_modules");
+
+const essentialPackages = new Set([
+  "react",
+  "react-dom",
+  "react-scripts", // For Create React App
+  "vite",
+  "parcel",
+  "@babel/core",
+  "@babel/preset-react",
+  "@emotion/react",
+  "@emotion/styled",
+  "@mui/material",
+]);
 
 function getPackageJson() {
   return JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
@@ -19,25 +32,47 @@ function getDependencies() {
   };
 }
 
+function analyzeFile(content) {
+  const usedPackages = new Set();
+
+  // Parsing with babel-parser to handle modern syntax
+  const ast = babelParser.parse(content, {
+    sourceType: "module",
+    plugins: ["jsx", "typescript"], // Enable plugins for JSX and TypeScript support
+  });
+
+  const traverse = (node) => {
+    if (node.type === "ImportDeclaration" || node.type === "CallExpression") {
+      const moduleName =
+        node.source?.value || node.arguments?.[0]?.value || null;
+
+      if (moduleName && typeof moduleName === "string") {
+        usedPackages.add(moduleName.split("/")[0]);
+      }
+    }
+
+    for (const key in node) {
+      if (node[key] && typeof node[key] === "object") {
+        traverse(node[key]);
+      }
+    }
+  };
+
+  traverse(ast);
+
+  return usedPackages;
+}
+
 function findUsedPackages() {
   const usedPackages = new Set();
   const files = glob.sync("**/*.{js,jsx,ts,tsx}", {
-    ignore: ["node_modules/**"],
+    ignore: ["node_modules/**", "**/test/**", "**/tests/**"],
   });
 
   files.forEach((file) => {
     const content = fs.readFileSync(file, "utf8");
-    const importRegex = /import\s+.*\s+from\s+['"]([^'"]+)['"]/g;
-    const requireRegex = /require\s*\(\s*['"]([^'"]+)['"]\s*\)/g;
-    let match;
-
-    while ((match = importRegex.exec(content)) !== null) {
-      usedPackages.add(match[1].split("/")[0]);
-    }
-
-    while ((match = requireRegex.exec(content)) !== null) {
-      usedPackages.add(match[1].split("/")[0]);
-    }
+    const packagesInFile = analyzeFile(content);
+    packagesInFile.forEach((pkg) => usedPackages.add(pkg));
   });
 
   return usedPackages;
@@ -46,19 +81,11 @@ function findUsedPackages() {
 function removeUnusedPackages(usedPackages) {
   const pkg = getPackageJson();
   const dependencies = Object.keys(pkg.dependencies || {});
-  const devDependencies = Object.keys(pkg.devDependencies || {});
 
   dependencies.forEach((dep) => {
-    if (!usedPackages.has(dep)) {
+    if (!usedPackages.has(dep) && !essentialPackages.has(dep)) {
       console.log(`Removing unused dependency: ${dep}`);
       delete pkg.dependencies[dep];
-    }
-  });
-
-  devDependencies.forEach((dep) => {
-    if (!usedPackages.has(dep)) {
-      console.log(`Removing unused devDependency: ${dep}`);
-      delete pkg.devDependencies[dep];
     }
   });
 
@@ -74,11 +101,10 @@ function clean() {
 
   console.log("Removing node_modules and reinstalling packages...");
 
-  // Cross-platform command for deleting node_modules
   if (process.platform === "win32") {
-    execSync("rmdir /s /q node_modules");
+    execSync("rmdir /s /q node_modules", { stdio: "inherit" });
   } else {
-    execSync("rm -rf node_modules");
+    execSync("rm -rf node_modules", { stdio: "inherit" });
   }
 
   execSync("npm install", { stdio: "inherit" });
